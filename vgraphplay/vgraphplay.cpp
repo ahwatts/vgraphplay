10fd38af9b5c3a886c0e5889555a1df57144a4fd
Andrew Watts <ahwatts@gmail.com>
2016-11-15 00:43:20 -0500

    Move stuff in to the class.

--- vgraphplay/vgraphplay.cpp
+++ vgraphplay/vgraphplay.cpp
@@ -10,161 +10,192 @@
 class VGraphplayApp {
 public:
     VGraphplayApp()
-        : instance{nullptr},
-          physical_device{}
+        : instance{VK_NULL_HANDLE},
+          physical_device{VK_NULL_HANDLE},
+          device{VK_NULL_HANDLE},
+          cmd_pool{VK_NULL_HANDLE},
+          cmd_buf{VK_NULL_HANDLE},
+          queue_family{UINT32_MAX}
     {}
 
     ~VGraphplayApp() {
-        vkFreeCommandBuffers(device, cmd_pool, 1, &cmd_buf);
-        vkDestroyCommandPool(device, cmd_pool, nullptr);
-        vkDestroyDevice(device, nullptr);
-        vkDestroyInstance(instance, nullptr);
-    }
+        if (device != VK_NULL_HANDLE && cmd_pool != VK_NULL_HANDLE && cmd_buf != VK_NULL_HANDLE) {
+            std::cout << "Freeing command buffer: " << cmd_buf << std::endl;
+            vkFreeCommandBuffers(device, cmd_pool, 1, &cmd_buf);
+        }
 
-    VkInstance instance = VK_NULL_HANDLE;
-    VkPhysicalDevice physical_device = VK_NULL_HANDLE;
-    VkDevice device = VK_NULL_HANDLE;
-    VkCommandPool cmd_pool = VK_NULL_HANDLE;
-    VkCommandBuffer cmd_buf = VK_NULL_HANDLE;
+        if (device != VK_NULL_HANDLE && cmd_pool != VK_NULL_HANDLE) {
+            std::cout << "Destroying command pool: " << cmd_pool << std::endl;
+            vkDestroyCommandPool(device, cmd_pool, nullptr);
+        }
 
-    uint32_t queue_family = UINT32_MAX;
-};
+        if (device != VK_NULL_HANDLE) {
+            std::cout << "Destroying (logical) device: " << device << std::endl;
+            vkDestroyDevice(device, nullptr);
+        }
 
-VkPhysicalDevice choosePhysicalDevice(VkInstance &instance) {
-    uint32_t num_devices = 0;
-    vkEnumeratePhysicalDevices(instance, &num_devices, nullptr);
-
-    std::vector<VkPhysicalDevice> devices(num_devices);
-    vkEnumeratePhysicalDevices(instance, &num_devices, devices.data());
-
-    for (auto&& device : devices) {
-        VkPhysicalDeviceProperties props;
-        vkGetPhysicalDeviceProperties(device, &props);
-
-        std::cout << "Physical device: " << props << std::endl;
-
-        // VkPhysicalDeviceMemoryProperties mem_props;
-        // vkGetPhysicalDeviceMemoryProperties(device, &mem_props);
-
-        // for (unsigned int i = 0; i < mem_props.memoryHeapCount; ++i) {
-        //     std::cout << "  Memory heap " << i << ": " << mem_props.memoryHeaps[i] << std::endl;
-        // }
-
-        // for (unsigned int i = 0; i < mem_props.memoryTypeCount; ++i) {
-        //     std::cout << "  Memory type " << i << ": " << mem_props.memoryTypes[i] << std::endl;
-        // }
-    }
-
-    // We probably want to make a better decision than this...
-    return devices[0];
-}
-
-uint32_t chooseQueueFamilyIndex(VkPhysicalDevice &device) {
-    uint32_t num_queue_families = 0;
-    vkGetPhysicalDeviceQueueFamilyProperties(device, &num_queue_families, nullptr);
-    std::vector<VkQueueFamilyProperties> queue_families(num_queue_families);
-    vkGetPhysicalDeviceQueueFamilyProperties(device, &num_queue_families, queue_families.data());
-
-    for (unsigned int i = 0; i < queue_families.size(); ++i) {
-        std::cout << "  Queue family " << i << ": " << queue_families[i] << std::endl;
-    }
-
-    for (unsigned int i = 0; i < queue_families.size(); ++i) {
-        if (queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-            return i;
+        if (instance != VK_NULL_HANDLE) {
+            std::cout << "Destroying instance: " << instance << std::endl;
+            vkDestroyInstance(instance, nullptr);
         }
     }
 
-    // ?
-    return UINT32_MAX;
-}
+    bool initInstance() {
+        VkInstanceCreateInfo create_info;
+        create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        create_info.pNext = nullptr;
+        create_info.pApplicationInfo = nullptr;
+        create_info.enabledLayerCount = 0;
+        create_info.ppEnabledLayerNames = nullptr;
+        create_info.enabledExtensionCount = 0;
+        create_info.ppEnabledExtensionNames = nullptr;
 
-void initVulkan(VGraphplayApp &app) {
-    VkInstanceCreateInfo vk_info;
-    VkResult rslt;
-
-    vk_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    vk_info.pNext = nullptr;
-    vk_info.pApplicationInfo = nullptr;
-    vk_info.enabledLayerCount = 0;
-    vk_info.ppEnabledLayerNames = nullptr;
-    vk_info.enabledExtensionCount = 0;
-    vk_info.ppEnabledExtensionNames = nullptr;
-    rslt = vkCreateInstance(&vk_info, nullptr, &app.instance);
-
-    if (rslt == VK_SUCCESS) {
-        std::cout << "Instance created: " << app.instance << std::endl;
-    } else {
-        std::cerr << "Error: " << rslt << " instance = " << app.instance << std::endl;
-        std::exit(1);
+        VkResult rslt = vkCreateInstance(&create_info, nullptr, &instance);
+        if (rslt == VK_SUCCESS) {
+            std::cout << "Instance created: " << instance << std::endl;
+            return true;
+        } else {
+            std::cerr << "Error: " << rslt << " instance = " << instance << std::endl;
+            return false;
+        }
     }
 
-    app.physical_device = choosePhysicalDevice(app.instance);
-    app.queue_family = chooseQueueFamilyIndex(app.physical_device);
+    bool initDevice() {
+        uint32_t num_devices = 0;
+        vkEnumeratePhysicalDevices(instance, &num_devices, nullptr);
 
-    float queue_priority = 1.0;
-    VkDeviceQueueCreateInfo queue_info;
-    queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-    queue_info.pNext = nullptr;
-    queue_info.flags = 0;
-    queue_info.queueFamilyIndex = app.queue_family;
-    queue_info.queueCount = 1;
-    queue_info.pQueuePriorities = &queue_priority;
+        std::vector<VkPhysicalDevice> devices(num_devices);
+        vkEnumeratePhysicalDevices(instance, &num_devices, devices.data());
 
-    VkDeviceCreateInfo device_info;
-    device_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-    device_info.pNext = nullptr;
-    device_info.queueCreateInfoCount = 1;
-    device_info.pQueueCreateInfos = &queue_info;
-    device_info.enabledExtensionCount = 0;
-    device_info.ppEnabledExtensionNames = nullptr;
-    device_info.enabledLayerCount = 0;
-    device_info.ppEnabledLayerNames = nullptr;
-    device_info.pEnabledFeatures = nullptr;
+        for (auto&& device : devices) {
+            VkPhysicalDeviceProperties props;
+            vkGetPhysicalDeviceProperties(device, &props);
 
-    rslt = vkCreateDevice(app.physical_device, &device_info, nullptr, &app.device);
+            std::cout << "Physical device: " << props << std::endl;
 
-    if (rslt == VK_SUCCESS) {
-        std::cout << "Device Created: " << app.device << std::endl;
-    } else {
-        std::cerr << "Error: " << rslt << " device = " << app.device << std::endl;
-        std::exit(1);
+            VkPhysicalDeviceMemoryProperties mem_props;
+            vkGetPhysicalDeviceMemoryProperties(device, &mem_props);
+
+            for (unsigned int i = 0; i < mem_props.memoryHeapCount; ++i) {
+                std::cout << "  Memory heap " << i << ": " << mem_props.memoryHeaps[i] << std::endl;
+            }
+
+            for (unsigned int i = 0; i < mem_props.memoryTypeCount; ++i) {
+                std::cout << "  Memory type " << i << ": " << mem_props.memoryTypes[i] << std::endl;
+            }
+        }
+
+        // We probably want to make a better decision than this...
+        physical_device = devices[0];
+
+        uint32_t num_queue_families = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &num_queue_families, nullptr);
+        std::vector<VkQueueFamilyProperties> queue_families(num_queue_families);
+        vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &num_queue_families, queue_families.data());
+
+        for (unsigned int i = 0; i < queue_families.size(); ++i) {
+            std::cout << "  Queue family " << i << ": " << queue_families[i] << std::endl;
+        }
+
+        // Choose the first graphics queue...
+        for (unsigned int i = 0; i < queue_families.size(); ++i) {
+            if (queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                queue_family = i;
+                break;
+            }
+        }
+
+        std::cout << "physical device = " << physical_device << std::endl
+                  << "queue family = " << queue_family << std::endl;
+
+        float queue_priority = 1.0;
+        VkDeviceQueueCreateInfo queue_info;
+        queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queue_info.pNext = nullptr;
+        queue_info.flags = 0;
+        queue_info.queueFamilyIndex = queue_family;
+        queue_info.queueCount = 1;
+        queue_info.pQueuePriorities = &queue_priority;
+
+        VkDeviceCreateInfo device_info;
+        device_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        device_info.pNext = nullptr;
+        device_info.queueCreateInfoCount = 1;
+        device_info.pQueueCreateInfos = &queue_info;
+        device_info.enabledExtensionCount = 0;
+        device_info.ppEnabledExtensionNames = nullptr;
+        device_info.enabledLayerCount = 0;
+        device_info.ppEnabledLayerNames = nullptr;
+        device_info.pEnabledFeatures = nullptr;
+
+        VkResult rslt = vkCreateDevice(physical_device, &device_info, nullptr, &device);
+
+        if (rslt == VK_SUCCESS) {
+            std::cout << "Device Created: " << device << std::endl;
+            return true;
+        } else {
+            std::cerr << "Error: " << rslt << " device = " << device << std::endl;
+            return false;
+        }
     }
 
-    VkCommandPoolCreateInfo cmd_pool_info;
-    cmd_pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
-    cmd_pool_info.pNext = nullptr;
-    cmd_pool_info.flags = 0;
-    cmd_pool_info.queueFamilyIndex = app.queue_family;
+    bool initCommandBuffer() {
+        VkCommandPoolCreateInfo cmd_pool_info;
+        cmd_pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+        cmd_pool_info.pNext = nullptr;
+        cmd_pool_info.flags = 0;
+        cmd_pool_info.queueFamilyIndex = queue_family;
 
-    rslt = vkCreateCommandPool(app.device, &cmd_pool_info, nullptr, &app.cmd_pool);
+        VkResult rslt = vkCreateCommandPool(device, &cmd_pool_info, nullptr, &cmd_pool);
 
-    if (rslt == VK_SUCCESS) {
-        std::cout << "Command pool created: " << app.cmd_pool << std::endl;
-    } else {
-        std::cerr << "Error: " << rslt << " command pool = " << app.cmd_pool << std::endl;
-        std::exit(1);
+        if (rslt == VK_SUCCESS) {
+            std::cout << "Command pool created: " << cmd_pool << std::endl;
+        } else {
+            std::cerr << "Error: " << rslt << " command pool = " << cmd_pool << std::endl;
+            return false;
+        }
+
+        VkCommandBufferAllocateInfo cmd_buf_info;
+        cmd_buf_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        cmd_buf_info.pNext = nullptr;
+        cmd_buf_info.commandPool = cmd_pool;
+        cmd_buf_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        cmd_buf_info.commandBufferCount = 1;
+
+        rslt = vkAllocateCommandBuffers(device, &cmd_buf_info, &cmd_buf);
+
+        if (rslt == VK_SUCCESS) {
+            std::cout << "Command buffer created: " << cmd_buf << std::endl;
+            return true;
+        } else {
+            std::cerr << "Error: " << rslt << " command buf = " << cmd_buf << std::endl;
+            return false;
+        }
     }
 
-    VkCommandBufferAllocateInfo cmd_buf_info;
-    cmd_buf_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-    cmd_buf_info.pNext = nullptr;
-    cmd_buf_info.commandPool = app.cmd_pool;
-    cmd_buf_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-    cmd_buf_info.commandBufferCount = 1;
+    VkInstance instance;
+    VkPhysicalDevice physical_device;
+    VkDevice device;
+    VkCommandPool cmd_pool;
+    VkCommandBuffer cmd_buf;
 
-    rslt = vkAllocateCommandBuffers(app.device, &cmd_buf_info, &app.cmd_buf);
-
-    if (rslt == VK_SUCCESS) {
-        std::cout << "Command buffer created: " << app.cmd_buf << std::endl;
-    } else {
-        std::cerr << "Error: " << rslt << " command buf = " << app.cmd_buf << std::endl;
-        std::exit(1);
-    }
-}
+    uint32_t queue_family;
+};
 
 int main(int argc, char **argv) {
     VGraphplayApp app;
-    initVulkan(app);
+
+    if (!app.initInstance()) {
+        std::exit(1);
+    }
+
+    if (!app.initDevice()) {
+        std::exit(1);
+    }
+
+    if (!app.initCommandBuffer()) {
+        std::exit(1);
+    }
+
     return 0;
 }
